5a626bf4fc90e23287447dee34909c9c0dcbadff
ジェヒョン <h94051987@gmail.com>
2023-10-21 13:31:40 +0900

    generate project

---
+++ src/main/java/com/blog/BlogApplication.java
@@ -0,0 +1,23 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String HelloWorld(){
+		return "Hello world";
+	}
+
+}
+
+
